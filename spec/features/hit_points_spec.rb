4a348c855106b666b810e5eb4d13de0278e7f6eb
Simone <simonecaplin@gmail.com>
2018-06-01 15:01:37 +0100

    test helper for sign in and play

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'players can see hit points' do
   scenario 'player 1 can see player2 hit points' do
-    visit('/')
-    fill_in('player1', with: 'Maggie')
-    fill_in('player2', with: 'Gulliver')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content("Gulliver: 60 HP")
   end
 end
--- spec/features/player_names_spec.rb
+++ spec/features/player_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'players can start a fight by entering their names' do
   scenario 'players can enter names and see them' do
-    visit('/')
-    fill_in('player1', with: 'Maggie')
-    fill_in('player2', with: 'Gulliver')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content 'Maggie VS Gulliver'
   end
 end
