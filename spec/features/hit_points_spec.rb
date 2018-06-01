f8028269321cde15dabf4ad978f6853ea221a340
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-06-01 14:32:36 +0100

    Refactor of Viewing Hit Points

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,10 +1,9 @@
 feature 'players can see hit points' do
-  scenario 'player 1 can see player2 hit points after attacking' do
+  scenario 'player 1 can see player2 hit points' do
     visit('/')
     fill_in('player1', with: 'Maggie')
     fill_in('player2', with: 'Gulliver')
     click_button('Submit')
-    click_button('Attack')
-    expect(page).to have_content("Maggie has 50 HP")
+    expect(page).to have_content("Gulliver: 60 HP")
   end
 end
